f7eaf285bb93bd73dd12d72eee19a5423097e3e3
Hovis <github@bunkercode.com>
2017-05-30 21:48:49 +0000

    add react-datepicker, moment, and moment i18n hack, still works

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import DatePicker from 'react-datepicker'
 
 class Reproducer extends React.Component {
 	render() {
 		let foobar = this.props.maybe ? __("foo") : __("bar")           
-		return <div>{foobar}</div>;                                                                              
+		return <div>{foobar}
+		<DatePicker />
+			</div>;                                                                              
 	}
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const I18nPlugin = require('i18n-webpack-plugin')
+const webpack = require('webpack')
 
 const languages = {
 	'en': null,
@@ -28,6 +29,15 @@ module.exports = Object.keys(languages).map(function(language) {
 		},
 		plugins: [
 			new I18nPlugin( languages[language] ),
+			new webpack.ContextReplacementPlugin(/^\.\/locale$/, context => {                        
+				if (!/\/moment\b/.test(context.context)) { return }                                    
+
+				Object.assign(context, { // context needs to be modified in place                      
+					// by default this loads ./.* from locale/, but we only want the languages we use    
+					// en-us is the default and is automatically included                                
+					regExp: /^\.\/(de|es|fr|hy-am|ja|ru)\./,                                             
+				})                                                                                     
+			}),                                                                                      
 		],
 	}
 })
