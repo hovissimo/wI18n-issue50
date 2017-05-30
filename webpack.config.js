d02e4bf430a4710d7507e1c23b2cd8474f668e2a
Hovis <github@bunkercode.com>
2017-05-30 22:13:17 +0000

    add css, sass, a few loaders, still working

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
-const path = require('path')
+const autoprefixer = require('autoprefixer');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const I18nPlugin = require('i18n-webpack-plugin')
+const path = require('path')
+const StatsPlugin = require('stats-webpack-plugin');
 
 const languages = {
 	'en': null,
 	'de': require('./de.json'),
 }
 
+const autoprefixerOpts = { browsers: ['last 2 versions'] };
+const postcssOpts = { plugins: [ autoprefixer(autoprefixerOpts)], sourceMap: true };
 
 module.exports = Object.keys(languages).map(function(language) {
 	return {
@@ -24,10 +29,30 @@ module.exports = Object.keys(languages).map(function(language) {
 						presets: ['es2015', 'react'],
 					}
 				},
+				{
+					test: /\.s?css$/,
+					loader: ExtractTextPlugin.extract({
+						fallback: 'style-loader',
+						use: [
+							{loader: 'css-loader', options: {sourceMap: true}},
+							{loader: 'postcss-loader', options: postcssOpts},
+							{loader: 'sass-loader', options: {sourceMap: true}},
+						],
+					}),
+				}
 			],
 		},
 		plugins: [
 			new I18nPlugin( languages[language] ),
+			new StatsPlugin(`manifest.json`, {
+				// We only need assetsByChunkName
+				chunkModules: false,
+				source: false,
+				chunks: false,
+				modules: false,
+				assets: true
+			}),
+			new ExtractTextPlugin('bundle.css'),
 		],
 	}
 })
